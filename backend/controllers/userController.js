73dc826e759c9de97950d54056dbb7819041489b
David John <davidajohn@gmail.com>
2021-01-17 19:24:48 +0000

    Added new user creation to the backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,39 @@ const authUser = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc    Register a new user
+// @route   POST 'api/users'
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password // password is hashed via middleware in userModel.js
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+});
+
 // @desc    Get user profile
 // @route   GET 'api/users/profile'
 // @access  Private
@@ -32,4 +65,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     res.send({ _id, name, email, isAdmin });
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
