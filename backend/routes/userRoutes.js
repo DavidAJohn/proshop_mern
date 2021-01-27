67d968bc6eff5db32ce12ce6ce73a8fa11c2007e
David John <davidajohn@gmail.com>
2021-01-27 15:26:31 +0000

    Added user deletion backend endpoint and route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser } from '../controllers/userController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
@@ -8,5 +8,6 @@ router.post('/login', authUser);
 router.route('/profile')
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile);
+router.route('/:id').delete(protect, isAdmin, deleteUser);
 
 export default router;
