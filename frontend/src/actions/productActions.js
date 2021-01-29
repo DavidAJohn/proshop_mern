c1b0720ba70d73b9221e960853f1d505ee25f981
David John <davidajohn@gmail.com>
2021-01-29 19:17:46 +0000

    Added the product creation functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -8,7 +8,10 @@ import {
     PRODUCT_DETAILS_FAIL,
     PRODUCT_DELETE_REQUEST,
     PRODUCT_DELETE_SUCCESS,
-    PRODUCT_DELETE_FAIL
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS,
+    PRODUCT_CREATE_FAIL
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
@@ -66,7 +69,7 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
             }
         };
 
-        await axios.delete(`/api/products/${id}/pay`, config);
+        await axios.delete(`/api/products/${id}`, config);
 
         dispatch({
             type: PRODUCT_DELETE_SUCCESS
@@ -75,6 +78,39 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DELETE_FAIL,
+            payload:
+                error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        });
+    }
+};
+
+export const createProduct = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_CREATE_REQUEST
+        });
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+
+        const { data } = await axios.post('/api/products', {}, config);
+
+        dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data
+        });
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_CREATE_FAIL,
             payload:
                 error.response && error.response.data.message
                 ? error.response.data.message
