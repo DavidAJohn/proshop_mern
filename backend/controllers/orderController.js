3cdf6a263f2c3cba6fee1196dbfdc5cafade5eee
David John <davidajohn@gmail.com>
2021-02-01 00:40:23 +0000

    Added backend endpoint to allow orders to be marked as sent

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -95,4 +95,22 @@ const getOrders = asyncHandler(async (req, res) => {
     res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getUserOrders, getOrders };
+// @desc    Update order to Sent
+// @route   GET 'api/orders/:id/sent'
+// @access  Private + Admin
+const updateOrderToSent = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+
+    if (order) {
+        order.isDelivered = true;
+        order.deliveredAt = Date.now();
+
+        const updatedOrder = await order.save();
+        res.json(updatedOrder);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getUserOrders, getOrders, updateOrderToSent };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,21 @@
 import express from 'express';
 const router = express.Router();
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
-import { addOrderItems, getOrderById, updateOrderToPaid, getUserOrders, getOrders } from '../controllers/orderController.js';
+import { 
+    addOrderItems, 
+    getOrderById, 
+    updateOrderToPaid, 
+    getUserOrders, 
+    getOrders, 
+    updateOrderToSent 
+} from '../controllers/orderController.js';
 
-router.route('/').post(protect, addOrderItems).get(protect, isAdmin, getOrders);
+router.route('/')
+    .post(protect, addOrderItems)
+    .get(protect, isAdmin, getOrders);
 router.route('/myorders').get(protect, getUserOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/sent').put(protect, isAdmin, updateOrderToSent);
 
 export default router;
