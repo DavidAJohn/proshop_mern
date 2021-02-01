441ab88fcfa37a8d176e5961434c3ed41ff2bdb4
David John <davidajohn@gmail.com>
2021-02-01 14:41:15 +0000

    Created backend route to add product reviews

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,21 @@
 import express from 'express';
 const router = express.Router();
-import { getProducts, getProductById, deleteProduct, createProduct, updateProduct } from '../controllers/productController.js';
+import { 
+    getProducts, 
+    getProductById, 
+    deleteProduct, 
+    createProduct, 
+    updateProduct, 
+    createProductReview 
+} from '../controllers/productController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 router.route('/')
     .get(getProducts)
     .post(protect, isAdmin, createProduct)
+
+router.route('/:id/reviews')
+    .post(protect, createProductReview)
 
 router.route('/:id')
     .get(getProductById)
