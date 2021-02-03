5df3b691289d85949929f6abd1ddc6d7192dd3a6
David John <davidajohn@gmail.com>
2021-02-03 19:08:46 +0000

    Made the Paginate component more re-usable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ const App = () => {
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/order/:id' component={AdminOrderScreen} />
           <Route path='/admin/productlist' component={ProductListScreen} exact />
-          <Route path='/admin/productlist/:pageNumber' component={ProductListScreen} exact />
+          <Route path='/admin/productlist/page/:pageNumber' component={ProductListScreen} exact />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/search/:keyword' component={HomeScreen} exact />
--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useLocation } from 'react-router-dom';
 
-const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+const Paginate = ({ pages, page, keyword = '' }) => {
+    const location = useLocation();
+    const path = location.pathname;
+    const baseURL = path.split('/page/')[0] === '/' ? '' : path.split('/page/')[0];
+
     return pages > 1 && (
         <Pagination>
             {[...Array(pages).keys()]
                 .map(x => (
-                    <LinkContainer key={x+1} 
-                        to={!isAdmin ? keyword 
-                            ? `/search/${keyword}/page/${x+1}` 
-                            : `/page/${x+1}` 
-                                : `/admin/productlist/${x+1}`
-                            } >
-                                <Pagination.Item active={x+1 === page}>{x+1}</Pagination.Item>
+                    <LinkContainer key={x} to={`${baseURL}/page/${x+1}`}>
+                        <Pagination.Item active={x+1 === page}>{x+1}</Pagination.Item>
                     </LinkContainer>
                 ))
             }
