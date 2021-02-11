a1c1f4202a3a9d2cffbaa547af453dd70784e5f3
David John <davidajohn@gmail.com>
2021-02-11 14:50:57 +0000

    Prepared for production build and deployment

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,10 +1,14 @@
 import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const adminPassword = process.env.ADMIN_PASSWORD || 'admin123456';
 
 const users = [
     {
         name: 'Admin User',
         email: 'admin@example.com',
-        password: bcrypt.hashSync('123456', 10),
+        password: bcrypt.hashSync(adminPassword, 10),
         isAdmin: true
     },
     {
